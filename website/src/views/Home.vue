b699d47d4d7305aba375f99dd307f41c6bdea377
ES-Alexander <sandman.esalexander@gmail.com>
2025-02-10 11:53:09 +1100

    website: Home.vue: include usage links for context

--- website/src/views/Home.vue
+++ website/src/views/Home.vue
@@ -1,4 +1,17 @@
 <template>
+  <v-card class="mx-auto" title="Usage">
+    <template v-slot:prepend>
+      <v-icon color="info" icon="mdi-information"></v-icon>
+    </template>
+    <v-card-text>
+      Extensions can be installed in <a href="https://blueos.cloud/">BlueOS</a>,
+      through the built in
+      <a
+        href="https://blueos.cloud/docs/latest/usage/advanced/#extensions-manager"
+        >Extensions Manager</a
+      >.
+    </v-card-text>
+  </v-card>
   <Counter />
 </template>
 
